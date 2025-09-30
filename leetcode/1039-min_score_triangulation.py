e0a4b377bce08a91728598f6bf95695da8547874
whopezx <1720966918@qq.com>
2025-09-30 11:52:38 +0800

    finish leetcode 2235

---
+++ leetcode/1039-min_score_triangulation.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
+
+        pass
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.minScoreTriangulation([3,7,4,5]))
---
+++ leetcode/2235-sum.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1+num2
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.sum(1,2))
