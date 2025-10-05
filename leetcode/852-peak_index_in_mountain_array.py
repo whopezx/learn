555ef0a284e8d0867045bd31429681cff33a8dce
whopezx <1720966918@qq.com>
2025-10-05 16:28:56 +0800

    finish leetcode 852 note

--- leetcode/852-peak_index_in_mountain_array.py
+++ leetcode/852-peak_index_in_mountain_array.py
@@ -2,10 +2,27 @@ from typing import List
 
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        return arr.index(max(arr))
+        # return arr.index(max(arr))
+
+        # 二分查找
+        left = 0
+        right = len(arr) - 2
+        while left < right:
+            mid = left + (right - left) // 2
+            # 若有相同的peak，返回最左侧的peak，注意山坡不能有平坦的地方
+            if arr[mid] >= arr[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+            # # 若有相同的peak，返回最右侧的peak
+            # if arr[mid] <= arr[mid + 1]:
+            #     left = mid + 1
+            # else:
+            #     right = mid
+        return right
 
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.peakIndexInMountainArray(arr = [0,2,1,0]))
+    print(s.peakIndexInMountainArray(arr = [0,1,2,2,2,1,0]))
